e44022fddfa2e1b132274f2f93a65a4f296978d4
Gabe Oleinik <gomusic14@gmail.com>
2021-03-25 12:02:36 -0400

    215 of 246 remaining

--- Koans/AboutStrings.cs
+++ Koans/AboutStrings.cs
@@ -128,7 +128,7 @@ broken line";
             var originalString = strA;
             var strB = "World";
             strA += strB;
-            Assert.Equal(FILL_ME_IN, originalString);
+            Assert.Equal("Hello, ", originalString);
 
             //What just happened? Well, the string concatenation actually
             //takes strA and strB and creates a *new* string in memory
@@ -145,14 +145,14 @@ broken line";
         {
             var world = "World";
             var str = String.Format("Hello, {0}", world);
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("Hello, World", str);
         }
 
         [Step(13)]
         public void AnyExpressionCanBeUsedInFormatString()
         {
             var str = String.Format("The square root of 9 is {0}", Math.Sqrt(9));
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("The square root of 9 is 3", str);
         }
 
         [Step(14)]
@@ -160,56 +160,56 @@ broken line";
         {
             //You can modify the value inserted into the result
             var str = string.Format("{0,3:}", "x");
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("  x", str);
         }
 
         [Step(15)]
         public void StringsCanBePaddedToTheRight()
         {
             var str = string.Format("{0,-3:}", "x");
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("x  ", str);
         }
 
         [Step(16)]
         public void SeparatorsCanBeAdded()
         {
             var str = string.Format("{0:n}", 123456);
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("123,456.000", str);
         }
 
         [Step(17)]
         public void CurrencyDesignatorsCanBeAdded()
         {
             var str = string.Format("{0:c}", 123456);
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("$123,456.00", str);
         }
 
         [Step(18)]
         public void NumberOfDisplayedDecimalsCanBeControlled()
         {
             var str = string.Format("{0:.##}", 12.3456);
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("12.35", str);
         }
 
         [Step(19)]
         public void MinimumNumberOfDisplayedDecimalsCanBeControlled()
         {
             var str = string.Format("{0:.00}", 12.3);
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("12.30", str);
         }
 
         [Step(20)]
         public void BuiltInDateFormatters()
         {
             var str = string.Format("{0:t}", DateTime.Parse("12/16/2011 2:35:02 PM", CultureInfo.InvariantCulture));
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("2:35 PM", str);
         }
 
         [Step(21)]
         public void CustomDateFormatters()
         {
             var str = string.Format("{0:t m}", DateTime.Parse("12/16/2011 2:35:02 PM", CultureInfo.InvariantCulture));
-            Assert.Equal(FILL_ME_IN, str);
+            Assert.Equal("P 35", str);
         }
         //These are just a few of the formatters available. Dig some and you may find what you need.
 
